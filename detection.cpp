7da4e942b1bcfd9e03be634fe2b5433f1ee3a919
hamuhei1412 <s17031@tokyo.kosen-ac.jp>
2019-07-02 17:10:26 +0900

    [add]Detection内に関数を定義

---
+++ detection.cpp
@@ -0,0 +1,9 @@
+#include "detection.h"
+
+Detection::Detection(int i = 1) : BoardPos(std::make_pair(std::make_pair(0,0),std::make_pair(0,0))){
+
+}
+
+std::vector<std::vector<float>> Detection::SingleDetect(){
+    
+} 
--- detection.h
+++ detection.h
@@ -1,5 +1,13 @@
 #include <librealsense2/rs.hpp> 
+#include <vector>
+#include <utility>
 
 class Detection{
-
+private:
+    int DetectNum;
+    std::vector<std::vector<float>> field;
+    const std::pair<std::pair<float, float>, std::pair<float, float>> BoardPos;
+public:
+    Detection(int i);
+    std::vector<std::vector<float>> SingleDetect();
 };
