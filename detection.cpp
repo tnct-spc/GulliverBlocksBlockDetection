acca41d0bfce4275b09319a347339c421cdfa28c
hamuhei1412 <s17031@tokyo.kosen-ac.jp>
2019-07-03 18:07:52 +0900

    [add]レゴ関連の定数を追加(とりあえず、値は仮の値

--- detection.cpp
+++ detection.cpp
@@ -1,9 +1,15 @@
 #include "detection.h"
 
-Detection::Detection(int i = 1) : BoardPos(std::make_pair(std::make_pair(0,0),std::make_pair(0,0))){
+//for now
+const std::pair<std::pair<float, float>, std::pair<float, float>> Detection::BoardPos = std::make_pair(std::make_pair(0, 0), std::make_pair(0, 0));
+const float Detection::LegoDepth = 0;
+const float Detection::LegoWidth = 0;
+const float Detection::LegoHeight = 0;
+
+Detection::Detection(int i = 1){
 
 }
 
-std::vector<std::vector<float>> Detection::SingleDetect(){
-    
+std::vector<std::tuple<int,int,int>> Detection::SingleDetect(){
+
 } 
