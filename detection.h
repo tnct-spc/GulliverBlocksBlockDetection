c40a3dedc1b1e8114be77e84a009cf9ec897165b
hamuhei1412 <s17031@tokyo.kosen-ac.jp>
2019-07-03 18:11:02 +0900

    [add]public

--- detection.h
+++ detection.h
@@ -6,8 +6,13 @@ class Detection{
 private:
     int DetectNum;
     std::vector<std::vector<float>> field;
-    const std::pair<std::pair<float, float>, std::pair<float, float>> BoardPos;
+
 public:
+    static const std::pair<std::pair<float, float>, std::pair<float, float>> BoardPos;
+    static const float LegoDepth;
+    static const float LegoWidth;
+    static const float LegoHeight;
+
     Detection(int i);
-    std::vector<std::vector<float>> SingleDetect();
+    std::vector<std::tuple<int,int,int>> SingleDetect(); //Widrh, Height, Depth
 };
