37d8621df0de767116ed2f13469b97edcbab9ef1
hamuhei1412 <s17031@tokyo.kosen-ac.jp>
2019-07-09 16:23:43 +0900

    [add]テスト用

--- manager.cpp
+++ manager.cpp
@@ -5,5 +5,15 @@ Manager::Manager(){
 }
 
 void Manager::RunSingleDetection(){
-    
+    Detection detection;
+    while(1){
+        std::pair<std::vector<std::tuple<int, int, int>>, std::vector<std::tuple<int,int,int>>> detect_info = detection.SingleDetect();
+        for(int i = 0;i < detect_info.first.size();i++){
+            std::cout<<std::get<0>(detect_info.first.at(i))<<" "<<std::get<1>(detect_info.first.at(i))<<" "<<std::get<2>(detect_info.first.at(i))<<std::endl;
+        }
+        std::cout<<"Added"<<std::endl;
+        for(int i = 0;i < detect_info.second.size();i++){
+            std::cout<<std::get<0>(detect_info.second.at(i))<<" "<<std::get<1>(detect_info.second.at(i))<<" "<<std::get<2>(detect_info.second.at(i))<<std::endl;
+        }
+    }
 }
--- manager.h
+++ manager.h
@@ -1,5 +1,6 @@
 #include "communication.h" 
 #include "detection.h"
+#include <iostream>
 
 class Manager{
 
