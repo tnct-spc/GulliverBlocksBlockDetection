99a0bed7c0cbd1c4749344a9a38fcd5df398b04b
hamuhei <s17031@tokyo.kosen-ac.jp>
2019-07-23 14:48:55 +0900

    リファクタリング

--- detection.cpp
+++ detection.cpp
@@ -10,7 +10,7 @@ std::vector<std::vector<double>> Detection::getDepth(){
 
 }
 
-std::pair<std::vector<std::tuple<int,int,int>>,std::vector<std::tuple<int,int,int>>> Detection::singleDetect(){
+std::pair<std::vector<std::tuple<int, int, int>>, std::vector<std::tuple<int, int, int>>> Detection::singleDetect(){
 }
 
 std::pair<std::pair<int, int>, std::pair<int, int>> Detection::detectBoard(){
