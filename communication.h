5db64c2af81968a36601bf7c6098313827fd17a1
hamuhei <s17031@tokyo.kosen-ac.jp>
2019-09-11 12:43:05 +0900

    [fix]いんでんと

--- communication.h
+++ communication.h
@@ -6,10 +6,10 @@
 
 class Communication{
 public:
-   void postJson(std::string url, std::pair<std::vector<std::tuple<int,int,int>>, std::vector<std::tuple<int,int,int>>> block_data);
+    void postJson(std::string url, std::pair<std::vector<std::tuple<int,int,int>>, std::vector<std::tuple<int,int,int>>> block_data);
 
-   bool isDetection(std::string url);
+    bool isDetection(std::string url);
 
 private:
-   static size_t writeCallback(void *contents, size_t size, size_t nmemb, void *userp);
+    static size_t writeCallback(void *contents, size_t size, size_t nmemb, void *userp);
 };
