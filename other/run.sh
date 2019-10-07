9bc500d4ff4749a771b026d2fb2a6b96c0c639dd
hamuhei4869 <s17031@tokyo.kosen-ac.jp>
2019-10-07 14:50:56 +0900

    Update run.sh

--- other/run.sh
+++ other/run.sh
@@ -1,2 +1,2 @@
 # このプログラムを/etc/rc.d/rc.localの最下層で実行
-~/GulliverBlocksBlockDetection/build/a.out >> ~/GulliverBlocksBlockDetection/log/log.txt
+/home/spc-jiyu-demo/GulliverBlocksBlockDetection/build/a.out >> /home/spc-jiyu-demo/GulliverBlocksBlockDetection/log/log.txt
